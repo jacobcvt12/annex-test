75b428d99b60f5cc40b6cf2fcee60e922c3eec6d
Jacob Carey <jacobcvt12@gmail.com>
2016-03-22 22:09:25 -0400

    Correct function

--- generate.R
+++ generate.R
@@ -1,7 +1,7 @@
 files <- 1000
 data.len <- 10000
 
-for (i in seq <- len(files)) {
+for (i in seq_len(files)) {
     data <- rnorm(data.len)
     saveRDS(data, file=file.path("data", paste0("data-", i, ".RDS")))
 }
